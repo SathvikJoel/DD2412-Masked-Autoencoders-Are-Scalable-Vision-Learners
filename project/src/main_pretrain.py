4cbf9dc1a6dd681a9ada03ad1e2a1467b887837b
mariasandahl97 <115632551+mariasandahl97@users.noreply.github.com>
2022-11-24 14:49:38 +0100

    m

--- project/src/main_pretrain.py
+++ project/src/main_pretrain.py
@@ -141,21 +141,21 @@ def main(args):
     dataset_train = datasets.CIFAR100(root=args.data_path, train=True, download=True, transform=transform_train)
     print(dataset_train)
 
-    # if True:  # args.distributed:
-    #     num_tasks = misc.get_world_size()
-    #     global_rank = misc.get_rank()
-    #     sampler_train = torch.utils.data.DistributedSampler(
-    #         dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
-    #     )
-    #     print("Sampler_train = %s" % str(sampler_train))
-    # else:
-    #     sampler_train = torch.utils.data.RandomSampler(dataset_train)
+    if True:  # args.distributed:
+        num_tasks = misc.get_world_size()
+        global_rank = misc.get_rank()
+        sampler_train = torch.utils.data.DistributedSampler(
+            dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
+        )
+        print("Sampler_train = %s" % str(sampler_train))
+    else:
+        sampler_train = torch.utils.data.RandomSampler(dataset_train)
 
-    # if global_rank == 0 and args.log_dir is not None:
-    #     os.makedirs(args.log_dir, exist_ok=True)
-    #     log_writer = SummaryWriter(log_dir=args.log_dir)
-    # else:
-    #     log_writer = None
+    if global_rank == 0 and args.log_dir is not None:
+        os.makedirs(args.log_dir, exist_ok=True)
+        log_writer = SummaryWriter(log_dir=args.log_dir)
+    else:
+        log_writer = None
 
     data_loader_train = torch.utils.data.DataLoader(
         dataset_train, sampler=sampler_train,
